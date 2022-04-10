36fbeca5ceef99cbc99a403d3e3857099100b5ee
FallAngel1337 <fallangel@protonmail.com>
2022-04-10 13:56:59 -0300

    Adding an output `dest`(destination) option

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ mod oauth2;
 enum Argument {
     Config,
     Format,
+    Dest,
 }
 
 impl FromStr for Argument {
@@ -16,30 +17,35 @@ impl FromStr for Argument {
         match s.trim().to_lowercase().as_ref() {
             "--config" | "-c" => Ok(Argument::Config),
             "--format" | "-f" => Ok(Argument::Format),
+            "--dest" => Ok(Argument::Dest),
             _ => Err("Invalid option".to_string()),
         }
     }
 }
 
 fn main() {
-    let default_config_file = "config.toml".to_string();
+    let default_config_file = Some("config.toml".to_string());
 
     let mut args = std::env::args().skip(1);
-    let mut opts: HashMap<Argument, String> = HashMap::new();
+    let mut opts: HashMap<Argument, Option<String>> = HashMap::new();
 
     while let Some(arg) = args.next() {
         if arg.starts_with("--") {
             let argument = Argument::from_str(&arg).unwrap();
-            opts.insert(argument, args.next().unwrap());
+            opts.insert(argument, args.next());
         }
     }
 
     let config_file = opts.get(&Argument::Config)
-        .unwrap_or(&default_config_file);
-    let services = api::Services::new(&config_file).unwrap();
+        .unwrap_or(&default_config_file).as_ref();
+
+    let services = api::Services::new(config_file.unwrap()).unwrap();
     
     let output_format = opts.get(&Argument::Format)
-        .map(|fmt| api::OutputFormats::from_str(fmt).unwrap());
-    services.statistics(output_format);
+        .map(|fmt| api::OutputFormat::from_str(fmt.as_ref().unwrap()).unwrap());
+
+    let dest = opts.get(&Argument::Dest).map(|path| path.as_ref().unwrap());
+    
+    services.statistics(output_format, dest);
 
 }
