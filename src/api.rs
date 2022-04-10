c0336c54d9d5a3b386de0e3177dba1bf29bac54c
FallAngel1337 <fallangel@protonmail.com>
2022-04-10 11:51:47 -0300

    Added a bunch of stuff in here (sorry)

--- src/api.rs
+++ src/api.rs
@@ -1,5 +1,4 @@
-use std::{io, fs};
-
+use std::{io::{self, Write}, fs, str::FromStr, collections::HashMap};
 use serde::{Serialize, Deserialize};
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -12,7 +11,38 @@ pub struct Service {
     params: Option<toml::value::Table>,
 }
 
+#[derive(Serialize, Deserialize, Debug)]
+struct Output {
+    service_name: String,
+    filters: HashMap<String, String>,
+}
+
+#[derive(Debug, Clone, Copy)]
+pub enum OutputFormats {
+    Json,
+}
+
+impl FromStr for OutputFormats {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s.trim().to_lowercase().as_ref() {
+            "json" => Ok(Self::Json),
+            _ => Err("`{s}` is an invalid output format".to_string()),
+        }
+    }
+}
+
 pub struct Services(Vec<Service>);
+
+impl Output {
+    fn new(service_name: String) -> Self {
+        Self {
+            service_name,
+            filters: HashMap::new(),
+        }
+    }
+}
 
 impl Service {
     fn generate_url(&self) -> String {
@@ -42,7 +72,7 @@ impl Service {
         }
     }
 
-    pub fn execute(&self) {
+    pub fn execute(self, format: Option<OutputFormats>) {
         let url = self.generate_url();
         let token = self.authenticate();
 
@@ -64,13 +94,38 @@ impl Service {
         };
         
         let json: serde_json::Value = serde_json::from_str(&content).unwrap();
-        println!("service_name = {:?}", self.service_name);
-        if let Some(filter) = &self.filter {
-            filter.iter().for_each(|(name, value)| {
-                println!("{name} = {}", json.pointer(value.as_str().unwrap()).unwrap());
+        let mut output = Output::new(self.service_name.clone());
+
+        if let Some(filter) = self.filter {
+            filter.into_iter().for_each(|(name, value)| {
+                output.filters.insert(name.clone(), json.pointer(value.as_str().unwrap())
+                .unwrap_or_else(|| {
+                    panic!("({name:?}, ({value:?})) trigged an error");
+                }).to_string());
+            });
+        } else {
+            json.as_object().unwrap().iter().for_each(|(k, v)| {
+                output.filters.insert(k.to_string(), v.to_string());
             });
         }
-        println!()
+
+        if let Some(format) = format {
+            let file_name = output.service_name.clone() + ".json";
+            let mut file = fs::File::create(file_name).unwrap();
+
+            match format {
+                OutputFormats::Json => {
+                    let content = serde_json::to_string(&output).unwrap();
+                    file.write_all(content.as_bytes()).unwrap();
+                },
+            }
+        } else {
+            println!("service_name = {:?}", output.service_name);
+            output.filters.iter().for_each(|(k, v)| {
+                println!("{k} = {v}");
+            });
+            println!();
+        }
     }
 
     fn authenticate(&self) -> Option<String> {
@@ -91,9 +146,9 @@ impl Services {
         Ok(Self(services))
     }
 
-    pub fn statistics(&self) {
-        for service in self.0.iter() {
-            service.execute();
+    pub fn statistics(self, format: Option<OutputFormats>) {
+        for service in self.0 {
+            service.execute(format);
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,45 @@
+use std::{str::FromStr, collections::HashMap};
+
 mod api;
 mod oauth2;
 
+#[derive(PartialEq, Eq, Debug, Clone, Copy, Hash)]
+enum Argument {
+    Config,
+    Format,
+}
+
+impl FromStr for Argument {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s.trim().to_lowercase().as_ref() {
+            "--config" | "-c" => Ok(Argument::Config),
+            "--format" | "-f" => Ok(Argument::Format),
+            _ => Err("Invalid option".to_string()),
+        }
+    }
+}
+
 fn main() {
-    let file = std::env::args().nth(1).unwrap_or_else(|| "config.toml".to_string());
-    let services = api::Services::new(&file).unwrap();
-    services.statistics();
+    let default_config_file = "config.toml".to_string();
+
+    let mut args = std::env::args().skip(1);
+    let mut opts: HashMap<Argument, String> = HashMap::new();
+
+    while let Some(arg) = args.next() {
+        if arg.starts_with("--") {
+            let argument = Argument::from_str(&arg).unwrap();
+            opts.insert(argument, args.next().unwrap());
+        }
+    }
+
+    let config_file = opts.get(&Argument::Config)
+        .unwrap_or(&default_config_file);
+    let services = api::Services::new(&config_file).unwrap();
+    
+    let output_format = opts.get(&Argument::Format)
+        .map(|fmt| api::OutputFormats::from_str(fmt).unwrap());
+    services.statistics(output_format);
+
 }
